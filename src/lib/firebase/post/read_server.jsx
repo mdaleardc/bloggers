9d7a0d63f66db467d9181cf96752c3cd18551170
mdaleardc <ismailaliamot@gmail.com>
2025-03-22 21:30:02 +0600

    bug fix try

--- src/lib/firebase/post/read_server.jsx
+++ src/lib/firebase/post/read_server.jsx
@@ -8,16 +8,11 @@ export const revalidate = 0; // Prevent Next.js from caching
 /*export const getAllPosts = async () => {
   return await getDocs(collection(db, "posts"), { source: "server" }).then((snaps) => snaps.docs.map((d) => ({ id: d.id, ...d.data() })));
 };*/
+
 export const getAllPosts = async () => {
-  const queryRef = collection(db, "posts");
-  
-  // Fetch all posts, but filter out any posts with an empty ID
-  return await getDocs(queryRef, { source: "server" })
-    .then((snaps) => 
-      snaps.docs
-        .filter((doc) => doc.id && doc.id.trim() !== '') // Filter out documents with empty ID
-        .map((d) => ({ id: d.id, ...d.data() }))
-    );
+  const q = query(collection(db, "posts"), where("id", "!=", "")); // Fetch posts where 'id' is not empty
+  return await getDocs(q, { source: "server" })
+    .then((snaps) => snaps.docs.map((d) => ({ id: d.id, ...d.data() })));
 };
 
 export const getAllPostsWithCategory = async (categoryId) => {
